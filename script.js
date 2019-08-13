6bdbb212073ffa2df36d51518bc5238d83bb339c
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-08-12 22:14:25 -0600

    added js functions textBackground and largeText

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+	textColor(); largeText();
+};
+
+
+function largeText () {
+	document.getElementById("myp2").style.fontSize = "xx-large";
+}
+
+function textColor () {
+	document.getElementById("myP").style.backgroundColor = "orange";
+}
+
